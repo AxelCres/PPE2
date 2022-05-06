f7391838e701c0c966ddc7cc22d69d6d136c9f2c
acres <acres@LAPTOP-CDUVP82V>
2022-05-06 14:57:44 +0200

    Ajoutez des fichiers projet.

---
+++ PPE2/Controllers/ArticleController.cs
@@ -0,0 +1,77 @@
+﻿using Blaguotech.Areas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blaguotech.Areas
+{
+    public class ArticleController : Controller
+    {
+        public static List<ArticleModel> Articles { get; set; } = new List<ArticleModel>
+            {
+                new ArticleModel
+                {
+                    Id = 1,
+                    Titre = "Best Of Coluche",
+                    Auteur = "Coluche",
+                    Note = 4.5m
+                },
+                new ArticleModel
+                {
+                    Id = 2,
+                    Titre = "Best Of Bigard",
+                    Auteur = "Jean Marie Bigard",
+                    Note = 4.0m
+                },
+                new ArticleModel
+                {
+                    Id = 3,
+                    Titre = "Best Of Kev Adams",
+                    Auteur = "Kev Adams",
+                    Note = 3.2m
+                }
+            };
+
+        public ArticleController()
+        {
+        }
+        public IActionResult Index()
+        {
+            return View(Articles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Create(ArticleModel article)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int id)
+        {
+            var article = Articles.Find(a => a.Id == id);
+            return View(article);
+        }
+        [HttpPost]
+        public IActionResult Edit (ArticleModel article)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(article);
+            }
+            Articles.RemoveAll(a => a.Id == article.Id);
+            Articles.Add(article);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(ArticleModel article)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+}
---
+++ PPE2/Models/ArticleModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blaguotech.Areas.Models
+{
+    public class ArticleModel
+    {
+        [Key, Range(1,1000)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Le champs est requis")]  
+        public string Titre { get; set; }
+        [Required(ErrorMessage = "Le champs est requis")]
+        public string Auteur { get; set; }
+        [Range(0, 5)]
+        public decimal Note { get; set; }
+    }
+}
